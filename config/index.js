89df769f10b014bb516585a7a32c949ea70e0253
yoncher <113170245+yoncher@users.noreply.github.com>
2022-09-09 17:13:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ export const config = {
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
+   * [oN_1c5lWtokb03GcdCxAnp3y7YcI]
    *  {
    *    // 想要发送的人的名字
    *    name: "老婆0",
@@ -28,18 +28,18 @@ export const config = {
    *   }, 
    * ]
    */
-  USERS: [
+  USERS: [oN_1c5lWtokb03GcdCxAnp3y7YcI]
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "oN_1c5lWtokb03GcdCxAnp3y7YcI",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
       // 所在省份
-      province: "广东",
+      province: "陕西",
       // 所在城市
-      city: "肇庆",
+      city: "咸阳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "陕西",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "咸阳",
 
   /** 重要节日相关 */
 
